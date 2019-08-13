3ece405f849345ecca7cf65d625fe5d28dfc7dd4
Andreev Andrey <andrey.kapitonov.96@gmail.com>
2019-08-13 16:06:41 +0300

    Delete deprecated constants

--- src/constains/UserConstains.js
+++
@@ -1,6 +0,0 @@
-// возможно нужно избавиться от /api/v1 на данном уровне
-export const AUTH_ROUTES = {
-    permissions: '/api/v1/auth/permissions',
-    login: '/api/v1/auth/login',
-    signup: '/api/v1/auth/signup'
-};
