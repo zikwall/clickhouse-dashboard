a68c5c21ab1ce07d622c1dedf85e9a0ba69d94cd
Andreev Andrey <andrey.kapitonov.96@gmail.com>
2019-09-05 11:36:25 +0300

    Add navbar component

---
+++ src/components/ui/navbar/docs/01-basic-navbar.js
@@ -0,0 +1,103 @@
+import React from "react";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+} from "../index";
+
+import {
+  Dropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem
+} from "../../dropdown";
+
+import {
+  InputGroup, InputGroupText, InputGroupAddon
+} from '../../input-group';
+
+import Collapse from '../../collapse';
+import FormInput from '../../form-input';
+import { Nav, NavItem, NavLink } from "../../nav";
+
+export default class NavExample extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggleDropdown = this.toggleDropdown.bind(this);
+    this.toggleNavbar = this.toggleNavbar.bind(this);
+
+    this.state = {
+      dropdownOpen: false,
+      collapseOpen: false
+    };
+  }
+
+  toggleDropdown() {
+    this.setState({
+      ...this.state,
+      ...{
+        dropdownOpen: !this.state.dropdownOpen
+      }
+    });
+  }
+
+  toggleNavbar() {
+    this.setState({
+      ...this.state,
+      ...{
+        collapseOpen: !this.state.collapseOpen
+      }
+    });
+  }
+
+  render() {
+    return (
+        <Navbar type="dark" theme="primary" expand="md">
+          <NavbarBrand href="#">React React</NavbarBrand>
+          <NavbarToggler onClick={this.toggleNavbar} />
+
+          <Collapse open={this.state.collapseOpen} navbar>
+            <Nav navbar>
+              <NavItem>
+                <NavLink active href="#">
+                  Active
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="#" disabled>
+                  Disabled
+                </NavLink>
+              </NavItem>
+              <Dropdown
+                  open={this.state.dropdownOpen}
+                  toggle={this.toggleDropdown}
+              >
+                <DropdownToggle nav caret>
+                  Dropdown
+                </DropdownToggle>
+                <DropdownMenu>
+                  <DropdownItem>Action</DropdownItem>
+                  <DropdownItem>Another action</DropdownItem>
+                  <DropdownItem>Something else here</DropdownItem>
+                </DropdownMenu>
+              </Dropdown>
+            </Nav>
+
+            <Nav navbar className="ml-auto">
+              <InputGroup size="sm" seamless>
+                <InputGroupAddon type="prepend">
+                  <InputGroupText>
+                    <FontAwesomeIcon icon={faSearch} />
+                  </InputGroupText>
+                </InputGroupAddon>
+                <FormInput className="border-0" placeholder="Search..." />
+              </InputGroup>
+            </Nav>
+          </Collapse>
+        </Navbar>
+    );
+  }
+}
