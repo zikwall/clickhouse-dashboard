4dffc87b72efef8f66661250334f544e9a498a6b
Andreev Andrey <andrey.kapitonov.96@gmail.com>
2019-09-05 11:33:20 +0300

    Add dropdown component

---
+++ src/components/ui/dropdown/docs/01-basic-dropdown.js
@@ -0,0 +1,34 @@
+import React from "react";
+import {
+  Dropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem
+} from "../index";
+
+export default class DropdownExample extends React.Component {
+  constructor(props) {
+    super(props);
+    this.toggle = this.toggle.bind(this);
+    this.state = { open: false };
+  }
+
+  toggle() {
+    this.setState(prevState => {
+      return { open: !prevState.open };
+    });
+  }
+
+  render() {
+    return (
+      <Dropdown open={this.state.open} toggle={this.toggle}>
+        <DropdownToggle>Dropdown</DropdownToggle>
+        <DropdownMenu>
+          <DropdownItem>Action</DropdownItem>
+          <DropdownItem>Another action</DropdownItem>
+          <DropdownItem>Something else here</DropdownItem>
+        </DropdownMenu>
+      </Dropdown>
+    );
+  }
+}
