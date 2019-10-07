a34a7169507f5cd551519b72b9c18b256c221cf9
Andreev Andrey <andrey.kapitonov.96@gmail.com>
2019-10-07 13:05:37 +0300

    :heavy_check_mark: export CH helper

--- src/utils/index.js
+++ src/utils/index.js
@@ -3,11 +3,13 @@ import * as Color from "./Color";
 import * as Math from "./Math";
 import * as Node from "./Node";
 import * as BaseObject from './Object';
+import * as ClickHouse from './ClickHouse';
 
 export {
     Color,
     Number,
     Math,
     Node,
-    BaseObject
+    BaseObject,
+    ClickHouse
 }
