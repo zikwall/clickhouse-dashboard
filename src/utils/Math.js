a949156f1f34109b35cd8723e63554d95d8a35fe
Andreev Andrey <andrey.kapitonov.96@gmail.com>
2019-10-07 13:56:27 +0300

    :heavy_check_mark: Add method convert 30-second intervals to hours

--- src/utils/Math.js
+++ src/utils/Math.js
@@ -9,3 +9,14 @@ export const uniqueId = () => {
 export const percentage = (total, current) => {
     return parseFloat((100 * current) / total).toFixed(2);
 };
+
+// тридцати секундки переводим в часы
+export const seconds30toMars = (duration, fixed = 2) => {
+    let hours = duration/120;
+
+    if (isNaN(hours)) {
+        return 0;
+    }
+
+    return hours.toFixed(fixed);
+};
