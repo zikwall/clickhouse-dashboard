3a34664e89c9506b349b2b969d76fe1289ee1325
Andreev Andrey <andrey.kapitonov.96@gmail.com>
2019-11-07 09:07:29 +0300

    :rocket: Add Object sortable function (extra reversing)

--- src/utils/Object.js
+++ src/utils/Object.js
@@ -1,6 +1,30 @@
 
 export const isEmpty = (object) => Object.entries(object) == 0 && object.constructor === Object;
 
+export const sortable = (obj, reverse) => {
+    let sortable = [];
+
+    for(let key in obj) {
+        if(obj.hasOwnProperty(key)) {
+            sortable.push([key, obj[key]]);
+        }
+    }
+
+    let reversed = (reverse) ? -1 : 1;
+
+    sortable.sort((a, b) => {
+        return reversed * (a[1] - b[1]);
+    });
+
+    let newObj = {};
+
+    for (let s in sortable) {
+        newObj[sortable[s][0]] = sortable[s][1];
+    }
+
+    return newObj;
+};
+
 export const sort = (array, callback, useCustom = false) => {
     if (typeof callback != 'function') {
         throw new Error('Ты че ахуел?');
