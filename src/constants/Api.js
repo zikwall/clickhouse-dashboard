0d7669a5982484c771679ea8e7f4c361739f0b9a
Анисимов Алексей <a.anisimov@info-link.ru>
2019-12-25 10:36:55 +0300

    убрал свой тестовый url

--- src/constants/Api.js
+++ src/constants/Api.js
@@ -1,5 +1,4 @@
 
-/*export const API_DOMAIN = !process.env.NODE_ENV || process.env.NODE_ENV === 'development'
+export const API_DOMAIN = !process.env.NODE_ENV || process.env.NODE_ENV === 'development'
     ? 'http://account.limehd.local'
-    : 'http://clh.limehd.tv';*/
-export const API_DOMAIN = 'http://clickhouse-server-statistic.local';
+    : 'http://clh.limehd.tv';
