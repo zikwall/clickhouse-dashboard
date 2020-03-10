fbf33658be4e1a42c27014287bc2bfd4d0b21f81
AndreyKapitonov <dj-on-ik@mail.ru>
2020-03-10 15:02:57 +0300

    Cleaning code

--- .babelrc.js
+++ .babelrc.js
@@ -8,8 +8,6 @@ const MODULE_RESOLVER = ["module-resolver", {
     }
 }];
 
-console.log(MODULE_RESOLVER);
-
 module.exports = {
     "presets": ["@babel/preset-env", "@babel/preset-react"],
     "plugins": [
